9789c99bf873f49951b606f03b3f358cba78d361
sjx <404950139@qq.com>
2019-09-01 18:07:13 +0800

    201909011807

---
+++ src/components/element/button/src/Button.vue
@@ -0,0 +1,26 @@
+<template>
+  <button
+    :class="[
+      'el-button',
+      type ? `el-button--${type}` : '',
+      {
+        'is-plain': plain
+      }
+    ]"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'ElButton',
+  props: {
+    type: {
+      type: String,
+      validator: val => ['primary', 'success', 'warning', 'danger', 'info', 'text'].includes(val),
+    },
+    plain: Boolean,
+  },
+};
+</script>
